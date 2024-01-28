109f7d68240f951fce68d76b66bf28348b695834
Igor-Vuk <igvukelic@gmail.com>
2024-01-28 05:18:19 +0100

    revert changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import react from "@vitejs/plugin-react"
 import { visualizer } from "rollup-plugin-visualizer"
 import { splitVendorChunkPlugin } from "vite"
-import vercel from "vite-plugin-vercel"
 
 const isCodeSandbox =
   "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env
 
 export default {
-  plugins: [react(), splitVendorChunkPlugin(), vercel(), visualizer()],
+  plugins: [react(), splitVendorChunkPlugin(), visualizer()],
   root: "src/",
   publicDir: "../public/",
   base: "./",
